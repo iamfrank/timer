471c73e2b446530dbf10264b1e2363050b4a2e11
Frank Thomsen Renard <iamfrank@users.noreply.github.com>
2017-04-14 10:32:21 +0200

    Update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -39,3 +39,21 @@ gn.init({
 function showAlpha(a) {
     $('#rose').attr('style', 'transform: rotate(' + a + 'deg)');
 }
+
+
+/*
+ * Register service worker
+ * (Does nothing at present. Is needed to display Chrome install web app banner.)
+ */
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('js/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
